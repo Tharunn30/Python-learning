aca69040dd17920a0a7f6f2347cbcffdd4df1906
P.V.Tharunn Raj <68377504+Tharunn30@users.noreply.github.com>
2024-12-10 21:08:06 +0530

    Delete exercise2_most.py

--- exercise2_most.py
+++
@@ -1,7 +0,0 @@
-first_number= float(input("Enter the first number: "))
-second_number= float(input("Enter the second number: "))
-Addition = first_number + second_number
-Subtraction = first_number - second_number
-Multiplication = first_number * second_number
-Division = first_number / second_number
-print(Addition, Subtraction, Multiplication, Division)
